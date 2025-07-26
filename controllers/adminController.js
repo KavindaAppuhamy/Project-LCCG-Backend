a5983f7af044fc9826c5c37c10148b48dd75e4c8
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-07-26 18:17:28 +0530

    added sendOtpEmail and updaed sendOtp code and postAdmins

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 import Admin from "../models/admin.js"
 import bcrypt from "bcryptjs";
 import Otp from "../models/otp.js";
-import { sendOtp } from "../controllers/otpController.js";
+import { sendOtpEmail } from "../controllers/otpController.js";
 
 export function isAdminValid(req) {
   if (!req.user) {
@@ -45,7 +45,7 @@ export function postAdmins(req, res) {
       });
 
       newOtp.save().then(() => {
-        sendOtp(user.email, otp); // Send OTP email
+        sendOtpEmail(user.email, otp.toString());  // Send OTP email
         res.json({ message: "Admin created successfully. OTP sent." });
       });
     })
