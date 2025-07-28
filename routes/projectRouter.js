829e57c6369c9eecfed04326ebd1f141938db057
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-07-28 18:59:13 +0530

    added viewAllProjects routes

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {createProject, getProject, deleteProject, updateProject} from "../controllers/projectController.js";
+import {createProject, getProject, deleteProject, updateProject, viewAllProjects} from "../controllers/projectController.js";
 
 const projectRouter = express.Router();
 
 projectRouter.put('/update/:id', updateProject);
 projectRouter.delete('/delete/:id', deleteProject);
 projectRouter.get('/get/:id', getProject);
+projectRouter.get('/all', viewAllProjects);
 projectRouter.post('/create', createProject);
 
 export default projectRouter;
