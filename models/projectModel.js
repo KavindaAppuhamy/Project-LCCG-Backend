db56edaaaa7f46fcb403dcbc1309d000968d75b9
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2025-07-28 18:19:07 +0530

    removed unused Mongoose import and changed 'date' field type from String to Date in Project model

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,39 +1,38 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema({
-
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    venue: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        required: true
-    },
-    time: {
-        type: String,
-        required: true
-    },
-    organizer: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  venue: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  organizer: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
 });
 
 const Project = mongoose.model("Project", projectSchema);
